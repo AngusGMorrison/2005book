2b21330f79425596c6543ae835fe928221671168
Angus Morrison <contact@angus-morrison.com>
2019-09-27 20:01:14 +0100

    Prepared ApplicationController to check user login status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  def authenaticated_user_id
+    session[:user_id]
+  end
+
+  private
+
+  def check_logged_in
+    head(:forbidden) unless authenticated_user_id
+  end
+
+
 end
