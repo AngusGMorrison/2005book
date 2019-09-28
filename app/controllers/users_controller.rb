ed39abd9d209ce3a1df0594f00b281864c2f4801
Angus Morrison <contact@angus-morrison.com>
2019-09-28 07:38:06 +0100

    Added skeleton for user validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,22 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = user.create(users_params)
+
+    if @user.valid?
+      # Create session
+      redirect_to user_path
+    else
+      render :new
+    end
+
   end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :mod, :email, :password)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,37 @@
 class User < ApplicationRecord
     belongs_to :mod
+
     has_many :sent_messages, foreign_key: :sender_id, class_name: :Message
     has_many :recevied_messages, foreign_key: :receiver_id, class_name: :Message 
 
     has_many :friendships
     has_many :friends, through: :friendships, source: :friend
+
     has_many :groups, foreign_key: :admin_id
+
+
+    has_secure_password
+
+
+    validates :name, :mod, :email, :password, {
+                presence: true,
+                message: "%{attribute} is required."
+              }
+
+    validates :name, {
+                exclusion: { 
+                  in: "Mark Zuckerberg",
+                  message: "%{attribute} can't be Mark Zuckerberg."
+                },
+                length: {
+                  in: 2..30,
+                  message: "%{attribute} must be 2-30 characters long."
+                }
+              }
+
+    validates :email, {
+                uniqueness: true,
+                message: "%{email} is already in use."
+              }
+
 end
