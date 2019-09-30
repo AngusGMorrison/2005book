bcb4bbd485fd96e745897ec661c70f5ac293a890
Angus Morrison <contact@angus-morrison.com>
2019-09-30 15:07:12 +0100

    Basic profile information displaying

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
   before_action :require_logged_in
 
   def show
-    @user = current_user
+    @current_user = current_user
     @profile = Profile.find_by(slug: params[:slug])
     @profile_owner = @profile.user
   end
