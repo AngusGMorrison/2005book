35027d310116c5d7713c679a5e23aedd422a5685
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-09-30 15:41:32 +0100

    Fixed Friendship validation

--- app/models/concerns/friendship/validations.rb
+++ app/models/concerns/friendship/validations.rb
@@ -4,7 +4,7 @@ module Friendship::Validations
     included do 
         
         def friendship_doesnt_already_exist?
-            if Friendship.find_by(user_id: self.user_id, friend_id: self.friend_id) || Friendship.find_by(user_id: self.friend_id, friend_id: self.user_id)
+            if !Friendship.find_by(user_id: self.user_id, friend_id: self.friend_id).nil? || !Friendship.find_by(user_id: self.friend_id, friend_id: self.user_id).nil?
                 self.errors.add(:friend_id, "A friendship already exists between these two users")
             end
         end
@@ -22,7 +22,7 @@ module Friendship::Validations
         validates :friend_id, presence: true
 
         validate :friendship_doesnt_already_exist?
-        
+
         validate :user_is_not_friend?
     end
 
--- spec/features/friendships_spec.rb
+++ spec/features/friendships_spec.rb
@@ -2,17 +2,43 @@ require 'rails_helper'
 
 RSpec.describe "Friendships", type: :feature do 
 
-    let(:fill_name) { fill_in "user_name", with: "Test User" }
-
-  
     before do
- 
+        @mod = Mod.create(name: "Test Mod")
         
+        i = 1
+        2.times do 
+            User.create(
+                name: "Test User #{i}",
+                mod_id: @mod.id,
+                email: "test#{i}@email.com",
+                password: "password",
+                accepted_terms: true)
+            i += 1
+        end
+        current_user = User.first
+        visit users_path
     end
   
-    it "allows site visitors to register" do  
-
+    it "allows the current user to add another user as a friend" do  
+        click_button("request_#{User.second.id}")
+        expect(Friendship.all.length).to eq(1)
+        expect(current_path).to eq(login_path)
     end
+
+    # it "allows a user to accept a friend request" do  
+    #     click_button("Send Friend Request")
+    #     expect(Friendship.all.length).to eq(1)
+    # end
+
+    # it "allows a user to delete a friend request" do  
+    #     click_button("Send Friend Request")
+    #     expect(Friendship.all.length).to eq(1)
+    # end
+
+    # it "allows a user to remove a friend" do  
+    #     click_button("Send Friend Request")
+    #     expect(Friendship.all.length).to eq(1)
+    # end
 
 
 
