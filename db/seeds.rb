47c837078cd189fbc6ef0f590af63ba29d77d6d5
Angus Morrison <contact@angus-morrison.com>
2019-10-04 14:01:53 +0100

    Added custom seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,8 +27,10 @@ Chain.delete_all
 
 # Create mods
 Mod.create(name: "GitPushGitPaid")
-Mod.create(name: "2b||!2b")
-Mod.create(name: "!false")
+Mod.create(name: "Lord of the Strings")
+Mod.create(name: "Git Hurts When IP")
+Mod.create(name: "Git Up and Pry Again")
+Mod.create(name: "Scrumbags")
 
 puts "#{Mod.all.length} mods created"
 
@@ -54,189 +56,60 @@ puts "#{PoliticalView.all.length} political views created"
 
 
 # Create users
-50.times do 
-    User.create(
-        name: Faker::Name.name.gsub(/[^a-zA-Z\-\' ]/, ""),
-        email: Faker::Internet.unique.email,
-        birthday: Faker::Date.birthday(min_age: 18, max_age: 80),
-        password: "123456",
-        mod_id: Mod.all.sample.id,
-        accepted_terms: true
-    )
-
-    Profile.create(
-      user_id: User.last.id,
-      sex: Faker::Gender.binary_type,
-      studies: Faker::Lorem.sentence(word_count: 2),
-      hometown: Faker::Address.city,
-      phone_number: Faker::PhoneNumber.phone_number,
-      screenname: Faker::Lorem.sentence(word_count: 1),
-      interested_in: Faker::Lorem.sentence(word_count: 2),
-      relationship_status: Faker::Lorem.sentence(word_count: 2),
-      political_view_id: PoliticalView.all.sample,
-      interests: Faker::Lorem.sentence(word_count: 5),
-      books: Faker::Lorem.sentence(word_count: 4),
-      movies: Faker::Lorem.sentence(word_count: 3),
-      music: Faker::Music.band,
-      websites: Faker::Internet.url,
-      about_me: Faker::Lorem.sentence(word_count: 10),
-      photo_url: photo_urls.sample
-    )
-
-    Profile.last.generate_slug
-end
-
-puts "#{User.all.length} users and #{Profile.all.length} profiles created"
-
-
-# Create groups
-10.times do
-    Group.create(name: Faker::Lorem.sentence(word_count: 2), description: Faker::Lorem.sentence(word_count: 10), admin_id: User.all.sample.id)
-end
-
-Group.create(name: "Dog Lovers", description: "Who doesn't love dogs?!", admin_id: User.all.sample.id)
-Group.create(name: "DnD", description: "We play DnD every Wednesday", admin_id: User.all.sample.id)
-
-puts "#{Group.all.length} groups created"
-
-
-# Create GroupUsers
-25.times do 
-  GroupUser.create(group_id: Group.all.sample.id, user_id: User.all.sample.id)
-end
-
-puts "#{GroupUser.all.length} people joined groups"
-
-
-# Give everyone 10 friends
-User.all.each do |user|
-    5.times do  
-      Friendship.find_or_create_by(user_1: user, user_2: User.all.sample)
-    end
-  
-    5.times do 
-      Friendship.find_or_create_by(user_1: User.all.sample, user_2: user)
-    end 
-end
-
-puts "#{Friendship.all.length} friendships created"
-
-
-# Create message chains
-10.times do 
-  Chain.create()
-end
-
-puts "#{Chain.all.length} chains started"
-
-# Give each chain two users and some messages
-Chain.all.each do |chain|
-  u1 = User.all.sample
-  u2 = u1.friends.sample
-
-  5.times do 
-    Message.create(chain_id: chain.id, sender_id: u1.id, receiver_id: u2.id, content: Faker::Lorem.sentence(word_count: 10))
-    Message.create(chain_id: chain.id, sender_id: u2.id, receiver_id: u1.id, content: Faker::Lorem.sentence(word_count: 10))
-  end
-end
-
-puts "#{Message.all.length} messages shared"
-
-
-# Test Profile No. 1 - Mark
-
 mark = User.create(
-    name: "Mark Zuckerburg",
-    email: "mark@facebook.com",
-    password: "password1",
-    mod_id: 1,
-    accepted_terms: true
+  name: "Mark Zuckerberg",
+  email: "mark@facebook.com",
+  birthday: DateTime.new(1984, 5, 14),
+  password: "password1",
+  mod_id: Mod.all.sample.id,
+  accepted_terms: true
 )
 
-mark_profile = Profile.create(
-  user_id: mark.id,
+profile = Profile.create(
+  user_id: User.last.id,
   sex: "Male",
-  studies: Faker::Lorem.sentence(word_count: 2),
+  studies: "Your message history",
+  hometown: "White Plains, NY",
   phone_number: Faker::PhoneNumber.phone_number,
-  screenname: Faker::Lorem.sentence(word_count: 1),
-  interested_in: Faker::Lorem.sentence(word_count: 2),
-  relationship_status: Faker::Lorem.sentence(word_count: 2),
+  screenname: "Zuckonit",
+  interested_in: "What you are doing.",
   political_view_id: 5,
-  interests: Faker::Lorem.sentence(word_count: 5),
-  movies: Faker::Lorem.sentence(word_count: 3),
-  music: Faker::Music.band,
-  websites: Faker::Internet.url,
-  about_me: Faker::Lorem.sentence(word_count: 10),
-  photo_url: photo_urls.sample
+  interests: "Human amusements that we humans enjoy",
+  books: "World Order, Why Nations Fail, Sapiens",
+  movies: "Anything but The Social Network",
+  music: "The soft hum of a server room",
+  websites: "www.facebook.com",
+  about_me: "*screams in dial-up*",
+  photo_url: "https://selftaught.blog/wp-content/uploads/2019/01/MarkZuckerberg.jpg"
 )
 
-mark_profile.generate_slug
-
-#Give Mark 20 friends
-10.times do 
-    Friendship.create(user_1_id: mark.id, user_2_id: User.all.sample.id)
-    Friendship.create(user_1_id: User.all.sample.id, user_2_id: mark.id)
-end
-
-puts "#{mark.name} has been created with #{mark.friends.length} friends."
-
-# Give Mark 5 friend requests
-5.times do 
-  FriendRequest.create(requestor_id: User.all.sample.id, receiver_id: mark.id)
-end
-
-# Get Mark to send 5 friend requests
-5.times do 
-  FriendRequest.create(requestor_id: mark.id, receiver_id: User.all.sample.id)
-end
-
-puts "#{mark.name} has sent #{mark.friend_requests.length} friend requests and received #{mark.friend_requests_as_receiver.length} friend requests."
-
-# Give Mark some message chains
-5.times do 
-  friend = mark.friends.sample
-  chain = Chain.create()
-  5.times do 
-    # message sent by Mark...
-    message = Message.create(chain_id: chain.id, sender_id: mark.id, receiver_id: friend.id, content: Faker::Lorem.sentence(word_count: 15) )
-    # message sent to Mark by friend...
-    Message.create(chain_id: chain.id, sender_id: friend.id, receiver_id: mark.id, content: Faker::Lorem.sentence(word_count: 10) )
-  end
-end
-
-puts "#{mark.name} is part of #{mark.chains.length} message chains. He has shared #{mark.sent_messages.length} and has received #{mark.received_messages.length} messages."
-
-
-# Test Profile No. 2 - Eduardo
+mark.profile.generate_slug
 
 eduardo = User.create(
-    name: "Eduardo Saverin",
-    email: "eduardo@facebook.com",
-    password: "password1",
-    mod_id: 1,
-    accepted_terms: true
+  name: "Eduardo Saverin",
+  email: "eduardo@notfacebook.com",
+  birthday: DateTime.new(1982, 3, 19),
+  password: "password1",
+  mod_id: Mod.all.sample.id,
+  accepted_terms: true
 )
 
-eduardo_profile = Profile.create(
-  user_id: eduardo.id,
+Profile.create(
+  user_id: User.last.id,
   sex: "Male",
-  studies: Faker::Lorem.sentence(word_count: 2),
-  phone_number: Faker::PhoneNumber.phone_number ,
-  screenname: Faker::Lorem.sentence(word_count: 1),
-  looking_for: Faker::Lorem.sentence(word_count: 1),
-  relationship_status: Faker::Lorem.sentence(word_count: 2),
-  political_view_id: 3,
-  interests: Faker::Lorem.sentence(word_count: 5),
-  movies: Faker::Lorem.sentence(word_count: 3),
-  music: Faker::Music.band,
-  websites: Faker::Internet.url,
-  about_me: Faker::Lorem.sentence(word_count: 10),
-  photo_url: photo_urls.sample
+  studies: "Tax evasion",
+  hometown: "São Paulo",
+  phone_number: Faker::PhoneNumber.phone_number,
+  screenname: "The Financier",
+  interested_in: "Women",
+  political_view_id: 5,
+  interests: "Renouncing my US citizenship.",
+  books: "Great Expectations",
+  movies: "The Social Network",
+  music: "Mark trying to testify before the Senate",
+  websites: "www.facebook.com",
+  about_me: "Hey guys, remember me?",
+  photo_url: "https://nyobetabeat.files.wordpress.com/2012/05/eduardo-saverin-thherichest-org.jpeg?quality=80"
 )
 
-eduardo_profile.generate_slug
-
-puts "#{eduardo.name} has been created with #{eduardo.friend_ids.length} friends."
-
-
-
+eduardo.profile.generate_slug
